4467e6c756720262c4898703a40062b820673d18
Uhkis <hakala.janne@gmail.com>
2017-04-20 20:26:41 +0300

    Only show git on the right prompt.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -24,20 +24,8 @@ function fish_prompt
   set -l green (set_color 00FF00)
   set -l purple (set_color af5fff)
 
-  # Configure __fish_git_prompt
-  set -g __fish_git_prompt_char_stateseparator ' '
-  set -g __fish_git_prompt_color 5fdfff
-  set -g __fish_git_prompt_color_flags df5f00
-  set -g __fish_git_prompt_color_prefix white
-  set -g __fish_git_prompt_color_suffix white
-  set -g __fish_git_prompt_showdirtystate true
-  set -g __fish_git_prompt_showuntrackedfiles true
-  set -g __fish_git_prompt_showstashstate true
-  set -g __fish_git_prompt_show_informative_status true
-
   # Line 1
   echo -n $white$green$USER'@'$__fish_prompt_hostname$white' '$blue(pwd)$turquoise
-  __fish_git_prompt " (%s)"
   echo
 
   # Line 2
