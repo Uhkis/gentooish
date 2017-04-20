c87ac2e067b1ac0241f03b8ce4b127a332adca82
Uhkis <hakala.janne@gmail.com>
2017-04-20 21:32:53 +0300

    More verbose changed/modified git statuses.

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,13 +1,16 @@
 function fish_right_prompt
+  set -l niceblue (set_color 2345aa)
+
   # Configure __fish_git_prompt
-  set -g __fish_git_prompt_char_stateseparator ' '
-  set -g __fish_git_prompt_color 5fdfff
+  set -g __fish_git_prompt_color 2345aa
   set -g __fish_git_prompt_color_flags df5f00
   set -g __fish_git_prompt_color_prefix white
   set -g __fish_git_prompt_color_suffix white
+  set -g __fish_git_prompt_char_untrackedfiles " untracked:"
+  set -g __fish_git_prompt_char_dirtystate "modified:"
+  set -g __fish_git_prompt_char_stateseparator "> "
   set -g __fish_git_prompt_showdirtystate true
   set -g __fish_git_prompt_showuntrackedfiles true
-  set -g __fish_git_prompt_showstashstate true
   set -g __fish_git_prompt_show_informative_status true
-  __fish_git_prompt " (%s)"
+  __fish_git_prompt "$niceblue<%s"
 end
